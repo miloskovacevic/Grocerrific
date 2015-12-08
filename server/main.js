7838946ec843f0814005979c35503fbb4549844b
Milos <miloscpt@gmail.com>
2015-12-08 22:34:22 +0100

    Mongoose implemented

--- server/main.js
+++ server/main.js
@@ -3,12 +3,13 @@ var express = require('express');
 var app = new express();
 var parser = require('body-parser');
 
+//baza...
+require('./database');
+
 //routes...
 app.get('/', function (req, res) {
     res.render('./../app/index.ejs', {});
 });
-
-require('./routes/items')(app);
 
 //static files serving...
 app.use(express.static(__dirname + '/../tmp'));
@@ -16,8 +17,11 @@ app.use(express.static(__dirname + '/../tmp'));
 app.use(parser.json());
 app.use(parser.urlencoded({extended: false}));
 
+require('./routes/items')(app);
+
 // port listening...
 app.listen(7777, function(){
     console.log('Server is listening on port 7777...');
 });
+
 
